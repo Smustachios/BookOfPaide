cec3cef66268e3d1e1e21578ea3ff6628140c6dc
Smustachios <rskeksju@gmail.com>
2023-07-10 15:22:54 +0300

    10-07-2023

--- Assets/Scripts/ReelSpinner.cs
+++ Assets/Scripts/ReelSpinner.cs
@@ -9,7 +9,9 @@ public class ReelSpinner : MonoBehaviour
     public Action expandingReelStopped;
 
     private readonly int preSpinLenght = 45;
-
+    private Vector3 offset = new Vector3(0, 0.25f);
+    private float prespinDelay = 0.002f;
+    private float spinDelay = 0.005f;
 
     // Spin main game reels
     public void SpinReels(int[] randomReelPos)
@@ -32,12 +34,12 @@ public class ReelSpinner : MonoBehaviour
         // Prespin each reel.
         for (int i = 0; i < preSpinLenght * 4; i++)
         {
-            reelTransform.position -= new Vector3(0, 0.25f);
-            yield return new WaitForSeconds(0.002f);
+            reelTransform.position -= offset;
+            yield return new WaitForSecondsRealtime(prespinDelay);
         }
 
         // Setup positions for animation.
-        Vector3 finalPos = new (reelTransform.position.x, -(randomReelPos[reelData.reelId] * 3) + 3);
+        Vector3 finalPos = new(reelTransform.position.x, -(randomReelPos[reelData.reelId] * 3) + 3);
         Vector3 startPos = finalPos - new Vector3(0, -reelData.animationOffset);
 
         reelTransform.position = startPos;
@@ -45,8 +47,8 @@ public class ReelSpinner : MonoBehaviour
         // Spin each reel to final position.
         for (int i = 0; i < reelData.animationOffset * 4; i++)
         {
-            reelTransform.position -= new Vector3(0, 0.25f);
-            yield return new WaitForSeconds(0.005f);
+            reelTransform.position -= offset;
+            yield return new WaitForSecondsRealtime(spinDelay);
         }
 
         // If last reel has finished spinning, let gameManager know and continue with spin sequence.
